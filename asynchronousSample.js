1dafdd7895e1bd0b197df0c732b6f2c2e95c2dd5
basyorist <basyorist@gmail.com>
2023-06-08 19:11:32 +0900

    apikadai

---
+++ asynchronousSample.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const util = require('util');
+
+const promisifyReadFile = util.promisify(fs.readFile);
+
+/*
+function main() {
+  let fileContent
+
+  fs.readFile('index.html', (err, data) => {
+    fileContent = data.toString()
+    console.log(fileContent)
+  })
+
+  console.log(fileContent)
+}
+*/
+
+/*
+const readFilePromise = promisifyReadFile('index.html')
+
+readFilePromise.then((data) => {
+  const fileContent = data.toString()
+  console.log(fileContent)
+})
+*/
+
+async function main() {
+  const data = await promisifyReadFile('index.html');
+  const fileContent = data.toString();
+  console.log(fileContent);
+}
+
+main();
---
+++ callbackSample.js
@@ -0,0 +1,23 @@
+function add(v1, v2) {
+  return v1 + v2;
+}
+
+function calculate(v1, v2, callback) {
+  return callback(v1, v2);
+}
+
+const addResult = calculate(1, 2, add);
+console.log(addResult);
+
+function multiply(v1, v2) {
+  return v1 * v2;
+}
+
+const multiplyResult = calculate(1, 2, multiply);
+console.log(multiplyResult);
+
+function hello() {
+  console.log('hello');
+}
+
+setTimeout(hello, 5000);
